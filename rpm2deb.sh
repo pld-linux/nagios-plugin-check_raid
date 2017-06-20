fd57e8ff9df0f6ecb9fc99987f462ce9ccd620aa
Elan Ruusamäe <glen@pld-linux.org>
2017-06-20 20:23:10 +0300

    deb: specify runtime dependencies

--- rpm2deb.sh
+++ rpm2deb.sh
@@ -2,12 +2,36 @@
 set -xe
 
 V=${1-4.0.6}
-R=${2:-1}
+R=${2:-2}
+N=nagios-plugin-check_raid
+PV=$N-$V
+D="libmonitoring-plugin-perl, libmodule-pluggable-perl"
 
-./builder -bb *.spec
-fakeroot alien --scripts -d -k ../RPMS/nagios-plugin-check_raid-$V-$R.noarch.rpm --generate
-cd nagios-plugin-check_raid-$V
-mv ./usr/share/perl5/vendor_perl/App ./usr/share/perl5/App
-fakeroot debian/rules binary
-cd ..
-rm -rf nagios-plugin-check_raid-$V
+clean() {
+	rm -rf nagios-plugin-check_raid-$V
+}
+
+rpm() {
+	./builder -bb *.spec
+}
+
+unpack() {
+	clean
+	fakeroot alien --scripts -d -k ../RPMS/nagios-plugin-check_raid-$V-$R.noarch.rpm --generate
+	cd nagios-plugin-check_raid-$V
+	mv ./usr/share/perl5/vendor_perl/App ./usr/share/perl5/App
+	rmdir ./usr/share/perl5/vendor_perl
+	cd -
+}
+
+deb() {
+	cd $PV
+	sed -i -e "/Depends: / s/$/, $D/" debian/control
+	fakeroot debian/rules binary
+	cd -
+}
+
+rpm
+unpack
+deb
+clean
